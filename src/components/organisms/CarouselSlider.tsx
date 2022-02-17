4fcf51eee0820824c0ebe7f494bd9df70acc292f
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2022-02-17 12:51:17 +0530

    text ticker changes

--- src/components/organisms/CarouselSlider.tsx
+++ src/components/organisms/CarouselSlider.tsx
@@ -4,9 +4,7 @@ import { isTab, normalize, screenWidth } from 'src/shared/utils'
 import { ImageArticle } from '../molecules'
 import { articleProps } from './ArticleSection'
 import { flatListUniqueKey } from '../../constants'
-import { useTheme } from 'src/shared/styles/ThemeProvider';
 import { HeadlinesSection } from 'src/components/organisms';
-import { HeadlinesSectionProps } from './headlinesSection/HeadlinesSection';
 import { LatestArticleDataType } from '~/redux/latestNews/types'
 
 type CarouselSliderProps = {
@@ -17,30 +15,39 @@ type CarouselSliderProps = {
 const CarouselSlider = ({
     tickerData, heroData
 }: CarouselSliderProps) => {
-    const { themeData } = useTheme()
-    const sampleHeadlinesSectionData: HeadlinesSectionProps = {
-        headlineTitle: "آخر الأخبار",
-        headlineTitleColor: themeData.secondaryDavyGrey,
-        barColor: themeData.primary,
-        headlineDescription: "فيما دخلت المساعي الرامية للتهدئة بين إسرائيل والفلسطينيين مراحلة متقدمة أمس، استمر التصعيد الميداني، في سباق الساعات الأخيرة قبل حسم مسار",
-        headlineDescriptionColor: themeData.secondaryDarkSlate,
-    }
-
+    let tickerCount = 0
     const sliderRef = useRef<FlatList<articleProps>>(null)
     const renderItem = ({ item, index }: { item: articleProps, index: number }) => {
         return <ImageArticle key={index} {...item}
-        containerStyle={isTab ? carouselSliderStyle.tabletImageStyle : carouselSliderStyle.imageStyle} />
+            containerStyle={isTab ? carouselSliderStyle.tabletImageStyle : carouselSliderStyle.imageStyle} />
+    }
+    const renderData = ({ }: { item: articleProps, index: number }) => {
+        tickerCount = tickerData.length
+        return (
+            <View style={carouselSliderStyle.headNewsContainer}>
+                {tickerCount &&
+                    <HeadlinesSection
+                        duration={10000}
+                        loop
+                        tickerData={tickerData} headlineTitle={''} headlineDescription={''}
+                    />
+                }
+            </View>
+        )
     }
 
     return (
         <View>
-            <View style={carouselSliderStyle.headNewsContainer}>
-                <HeadlinesSection
-                    duration={10000}
-                    loop
-                    {...sampleHeadlinesSectionData}
-                />
-            </View>
+            <FlatList
+                data={tickerData}
+                keyExtractor={(_, index) => index.toString()}
+                listKey={flatListUniqueKey.CAROUSEL_WIDGET}
+                horizontal={true}
+                pagingEnabled={true}
+                showsHorizontalScrollIndicator={false}
+                renderItem={renderData}
+                bounces={false}
+            />
             <FlatList
                 ref={sliderRef}
                 data={heroData}
--- src/components/organisms/headlinesSection/HeadlinesSection.tsx
+++ src/components/organisms/headlinesSection/HeadlinesSection.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { normalize } from 'src/shared/utils';
 import TextTicker from 'react-native-text-ticker';
 import { Label, LabelTypeProp } from 'src/components/atoms';
-import { colors } from 'src/shared/styles/colors';
+import { LatestArticleDataType } from '~/redux/latestNews/types';
+import { ScreensConstants } from 'src/constants';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { useTheme } from 'src/shared/styles/ThemeProvider';
 
 interface TextTickerProps {
     duration?: number,
     loop?: boolean,
     repeatSpacer?: number,
     marqueeDelay?: number,
+    tickerData?: LatestArticleDataType[]
 }
-
 export interface HeadlinesSectionProps extends TextTickerProps {
     headlineTitle: string,
     headlineTitleColor?: string,
@@ -22,36 +26,97 @@ export interface HeadlinesSectionProps extends TextTickerProps {
     loop?: boolean,
     repeatSpacer?: number,
     marqueeDelay?: number,
+    tickerData: LatestArticleDataType[]
 }
 
 const HeadlinesSection = ({
-    headlineTitle,
     headlineTitleColor,
     barColor,
-    headlineDescription,
     headlineDescriptionColor,
     duration,
     loop,
     repeatSpacer,
     marqueeDelay,
+    tickerData
 }: HeadlinesSectionProps) => {
-    const titleColor = headlineTitleColor ? headlineTitleColor : colors.davyGrey;
-    const bodyColor = headlineDescriptionColor ? headlineDescriptionColor : colors.davyGrey;
-    const separatorColor = barColor ? barColor : colors.greenishBlue;
+    const navigation = useNavigation<StackNavigationProp<any>>()
+    const [headNews, setHeadNews] = React.useState('');
+    const [headerNews, setHeaderNews] = React.useState('');
+    const [indexValue, setIndexValue] = React.useState(0)
+    const [textWidth, setTextWidth] = React.useState(0);
+    const [titleWidth, setTitleWidth] = React.useState(0);
+    const { themeData } = useTheme()
+    const titleColor = headlineTitleColor ? headlineTitleColor : themeData.secondaryDavyGrey;
+    const bodyColor = headlineDescriptionColor ? headlineDescriptionColor : themeData.secondaryDarkSlate;
+    const separatorColor = barColor ? barColor : themeData.primary;
+    useEffect(() => {
+        setHeadNews(tickerData[0].title as any ?? '')
+        setHeaderNews(tickerData[0].news_categories.title as any ?? '')
+        setIndexValue(1)
+        return () => { }
+    }, [])
+
+    const getNextData = () => {
+        if (indexValue + 1 === tickerData.length) {
+            { setIndexValue(indexValue - indexValue) }
+        }
+        else {
+            setIndexValue(indexValue + 1)
+        }
+        return (
+            <>
+                {setHeadNews(tickerData[indexValue].title as any ?? '')}
+                {setHeaderNews(tickerData[indexValue].news_categories.title as any ?? '')}
+            </>
+        )
+    }
+    const isOverLapped = () => {
+        if (textWidth + titleWidth + 20 < Dimensions.get('window').width) {
+            setTimeout(function () {
+                { getNextData() }
+            }, duration);
+        }
+    }
+    const onPress = () => {
+        if (tickerData[indexValue].nid) {
+            navigation.navigate(ScreensConstants.ARTICLE_DETAIL_SCREEN, { nid: tickerData[indexValue].nid })
+        }
+    }
+    const renderData = () => {
+        return (
+            <View style={HeadlinesSectionStyle.contentContainer}>
+                <Label color={titleColor} children={headerNews} labelType={LabelTypeProp.p5} onLayout={e => { setTitleWidth(e.nativeEvent.layout.width) }} />
+                {tickerData[indexValue].news_categories.title as any && <View style={[HeadlinesSectionStyle.separator, { backgroundColor: separatorColor }]} />}
+                <TextTicker
+                    style={[HeadlinesSectionStyle.headlineDescription, { color: bodyColor }]}
+                    duration={duration ? duration : TextTickerDefaultProps.duration}
+                    loop={loop ? loop : TextTickerDefaultProps.loop}
+                    repeatSpacer={repeatSpacer ? repeatSpacer : TextTickerDefaultProps.repeatSpacer}
+                    marqueeDelay={marqueeDelay ? marqueeDelay : TextTickerDefaultProps.repeatSpacer}
+                    isRTL
+                    onMarqueeComplete={() => getNextData()}
+                >
+                    <Text
+                        onPress={onPress}>
+                        {headNews}
+                    </Text>
+
+                </TextTicker>
+                <View
+                    style={{ height: 0, alignSelf: 'flex-start' }}
+                    onLayout={e => {
+                        setTextWidth(e.nativeEvent.layout.width);
+                    }}>
+                    <Text style={HeadlinesSectionStyle.headlineDescription}>{headNews}</Text>
+                </View>
+                {isOverLapped()}
+            </View>
+        )
+    }
+
     return (
         <View style={HeadlinesSectionStyle.contentContainer}>
-            <Label color={titleColor} children={headlineTitle} labelType={LabelTypeProp.p5} />
-            {headlineTitle && <View style={[HeadlinesSectionStyle.separator, { backgroundColor: separatorColor }]} />}
-            <TextTicker
-                style={[HeadlinesSectionStyle.headlineDescription, { color: bodyColor }]}
-                duration={duration ? duration : TextTickerDefaultProps.duration}
-                loop={loop ? loop : TextTickerDefaultProps.loop}
-                repeatSpacer={repeatSpacer ? repeatSpacer : TextTickerDefaultProps.repeatSpacer}
-                marqueeDelay={marqueeDelay ? marqueeDelay : TextTickerDefaultProps.repeatSpacer}
-                isRTL
-            >
-                {headlineDescription}
-            </TextTicker>
+            {renderData()}
         </View>
     )
 }
@@ -59,7 +124,7 @@ const HeadlinesSection = ({
 const TextTickerDefaultProps = {
     style: {},
     duration: 3000, // in milliseconds
-    loop: false, 
+    loop: false,
     marqueeDelay: 1000, // in milliseconds
     repeatSpacer: 50, // The space between the end of the text string ticker and the beginning of it starting again
 }
@@ -74,7 +139,7 @@ const HeadlinesSectionStyle = StyleSheet.create({
         overflow: 'hidden',
     },
     separator: {
-        width: '.3%',
+        width: normalize(2),
         height: '45%',
         marginHorizontal: normalize(8)
     },
