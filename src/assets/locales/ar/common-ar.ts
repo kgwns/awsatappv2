7cd4082aa8ce61734af86a8ce96aceef087056c1
Mandeep <mandeep.s@tringapps.com>
2022-03-21 15:19:34 +0530

    updated password validation limit to 5

--- src/assets/locales/ar/common-ar.ts
+++ src/assets/locales/ar/common-ar.ts
@@ -168,7 +168,7 @@ export const arabic = {
     passwordStrength: 'Password is too short',
     passwordRequirement:'Password must contain (1)uppercase,(1)lowercase,(1)numeric and (1)special character',
     maxLengthPassword: 'Password should contain maximum 20 characters',
-    minLengthPassword: 'Password should contain minimum 8 characters',
+    minLengthPassword: 'Password should contain minimum 5 characters',
   },
   about_the_news_paper: 'عن الجريدة',
   terms_of_use: 'شروط الانتفاع',
