5d536cb9b7927e7b0a04874f23b3cb905ecf10e6
deepaktringapps <deepakkumar.r@tringapps.com>
2022-03-22 17:10:05 +0530

    Updated the add/remove bookmark in hooks

--- src/redux/bookmark/types.ts
+++ src/redux/bookmark/types.ts
@@ -67,7 +67,7 @@ export interface GetBookmarkIdType {
 
 export interface BookmarkIdSuccessDataFieldType {
   nid: string,
-  bundle: string
+  bundle?: string
 }
 
 export interface GetBookMarkIdSuccessMessageType {
