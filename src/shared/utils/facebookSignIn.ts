1e86c7283d1779e0d8680630feaff67571a992f1
Mandeep <mandeep.s@tringapps.com>
2022-03-22 13:42:58 +0530

    code refactor

--- src/shared/utils/facebookSignIn.ts
+++ src/shared/utils/facebookSignIn.ts
@@ -1,6 +1,5 @@
 import { LoginManager, AccessToken, Settings, GraphRequest, GraphRequestManager  } from "react-native-fbsdk-next";
 import SocialLogin from './socialLogin';
-import { store } from 'src/redux/store';
 
 export default class SignInFacebook extends SocialLogin {
   fbUserId: string;
@@ -17,8 +16,7 @@ export default class SignInFacebook extends SocialLogin {
   login(): void {
       LoginManager.logInWithPermissions(['public_profile', 'email', 'user_birthday']).then((result: any) => {
           if (result.isCancelled) {
-              //store.dispatch(socialLoginEnded());
-              console.log('** Facebook Login Canceled **')
+              //console.log('** Facebook Login Canceled **')
               this.callBack(undefined, false, 'facebook')
           } else {
               AccessToken.getCurrentAccessToken().then((data: any) => {
