c5260a50415461f8cfffecd080e4958a1e075b02
deepaktringapps <deepakkumar.r@tringapps.com>
2022-04-06 14:52:50 +0530

    Fixed the scroll issue for the section screen when paginate

--- src/components/screens/sectionArticles/SectionArticlesScreen.tsx
+++ src/components/screens/sectionArticles/SectionArticlesScreen.tsx
@@ -1,16 +1,19 @@
 import React, {useEffect, useState} from 'react';
 import {MostReadList} from 'src/components/organisms';
 import {ScreenContainer} from '..';
-import { horizontalAndTop } from 'src/shared/utils';
+import { horizontalAndTop, isNonEmptyArray, normalize } from 'src/shared/utils';
 import {useSectionArticles} from 'src/hooks';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {SectionArticlesBodyGet} from 'src/redux/sectionArticles/types';
 import { LoadingState } from 'src/components/atoms';
+import { ActivityIndicator, View } from 'react-native';
+import { useTheme } from 'src/shared/styles/ThemeProvider';
 
 export const SectionArticlesScreen = () => {
   const {params} = useRoute<RouteProp<any>>();
   const [page, setPage] = useState(0);
 
+  const { themeData } = useTheme()
   const {
     isLoading,
     sectionArticlesData,
@@ -32,18 +35,27 @@ export const SectionArticlesScreen = () => {
   }, [page]);
 
   const onScroll = () => {
-    setPage(page + 1);
+    if (!isLoading) {
+      setPage(page + 1);
+    }
   };
 
   return (
     <ScreenContainer edge={horizontalAndTop} showHeader={true} headerTitle={params?.title}>
       {
-      isLoading ? <LoadingState/> :
-        <MostReadList
-          data={sectionArticlesData}
-          onScroll={onScroll}
-          isLoading={isLoading}
-        />
+      (isLoading && sectionArticlesData && !isNonEmptyArray(sectionArticlesData.rows)) ? <LoadingState/> :
+          <>
+            <MostReadList
+              data={sectionArticlesData}
+              onScroll={onScroll}
+              isLoading={isLoading}
+            />
+            {(isLoading && sectionArticlesData && isNonEmptyArray(sectionArticlesData.rows)) &&
+            <View style={{ margin: normalize(28) }}>
+                <ActivityIndicator size={'small'} color={themeData.primary} />
+            </View>
+            }
+          </>
       }
     </ScreenContainer>
   );
