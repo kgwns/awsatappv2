599a11ea55a0f764b92cb0fa158a7ff3ad5b9236
deepaktringapps <60640600+deepaktringapps@users.noreply.github.com>
2022-07-11 18:50:52 +0530

    Removed related article section and suthor name (#686)

--- src/components/screens/articleDetail/ArticleDetailScreen.tsx
+++ src/components/screens/articleDetail/ArticleDetailScreen.tsx
@@ -170,7 +170,8 @@ export const ArticleDetailScreen = ({
           ...item,
           ...shortArticleWithTagProperties,
           titleColor: themeData.primaryBlack,
-          flag: item.news_categories && item.news_categories.title,
+          // flag: item.news_categories && item.news_categories.title, // AMAR-801 Removed section title 
+          author: '', // AMAR-801 Removed section title 
           isBookmarked: validateBookmark(item.nid)
         }
       })
