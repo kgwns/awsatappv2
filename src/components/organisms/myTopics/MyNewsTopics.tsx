e3545bc4d2077b38705a814384c58b7fee612cfc
deepaktringapps <deepakkumar.r@tringapps.com>
2022-08-01 15:15:53 +0530

    Updated the image field to display in my news screen

--- src/components/organisms/myTopics/MyNewsTopics.tsx
+++ src/components/organisms/myTopics/MyNewsTopics.tsx
@@ -7,7 +7,7 @@ import { Label, LabelTypeProp, LoadingState } from 'src/components/atoms'
 import { useIsFocused } from '@react-navigation/native'
 import { AllSiteCategoriesBodyGet } from 'src/redux/allSiteCategories/types'
 import { ArticlesListItemType, FavouriteArticlesBodyGet } from 'src/redux/contentForYou/types'
-import { dateTimeAgo, horizontalEdge, isObjectNonEmpty, TimeIcon } from 'src/shared/utils/utilities'
+import { dateTimeAgo, getImageUrl, horizontalEdge, isObjectNonEmpty, TimeIcon } from 'src/shared/utils/utilities'
 import { fonts } from 'src/shared/styles/fonts'
 import { colors, CustomThemeType } from 'src/shared/styles/colors'
 import { useThemeAwareObject } from 'src/shared/styles/useThemeAware'
@@ -150,7 +150,7 @@ export const MyNewsTopics = () => {
                 <ArticleItem
                     index={index}
                     nid={item.nid}
-                    image={item.field_new_photo || 'placeholderImg'}
+                    image={getImageUrl(item.field_new_photo) || 'placeholderImg'}
                     imageStyle={isTab ? styles.tabImageStyle : styles.imageStyle}
                     tagName={tagName}
                     title={item.title}
