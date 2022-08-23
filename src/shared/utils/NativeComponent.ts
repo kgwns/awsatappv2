99f7203428749df5a4f69c50272e4615de4467bd
deepaktringapps <deepakkumar.r@tringapps.com>
2022-08-23 16:21:30 +0530

    Removed commented code

--- src/shared/utils/NativeComponent.ts
+++ src/shared/utils/NativeComponent.ts
@@ -1,5 +1,5 @@
 import { requireNativeComponent } from "react-native"
 
 export const getRequiredNativeComponent = (name: string) => {
-    // return requireNativeComponent(name)
+    return requireNativeComponent(name)
 }
