024dfa75c2de675bba97c48b439f289218585da2
deepaktringapps <deepakkumar.r@tringapps.com>
2022-08-25 12:03:23 +0530

    Fixed font not increasing issue

--- src/components/screens/articleDetail/ArticleDetailScreen.tsx
+++ src/components/screens/articleDetail/ArticleDetailScreen.tsx
@@ -487,8 +487,6 @@ export const ArticleDetailScreen = ({
           removeClippedSubviews={false}
           onScroll={onScroll}
           scrollEnabled={scrollEnabled}
-          initialNumToRender={1}
-          maxToRenderPerBatch={1}
         />
         {isNotEmpty(articleDetailState[0].jwplayerId) && playerUrl && !isFullScreen && 
           <DraggableVideoPlayer videoRefs={videoRefs} setMiniPlayerVisible={closeMiniPlayer} url={playerUrl}
