2bde3da0c1c1039c3d77aa1c17b4b105b1d6f8d4
vigneshrajs <vigneshraj.s@tringapps.com>
2022-10-30 13:11:14 +0530

    amar_1044, videoDetail api added

---
+++ src/services/videoDetailService.ts
@@ -0,0 +1,48 @@
+import { AxiosError } from 'axios';
+import { getCacheApiRequest } from 'src/services/api';
+import { BASE_URL } from 'src/services/apiUrls';
+import { VideoItemType } from 'src/redux/videoList/types';
+import { VIDEO_DETAIL_ENDPOINT } from './apiEndPoints';
+import { isNonEmptyArray } from 'src/shared/utils';
+
+export interface FetchVideoDetailPayload {
+    nid: string;
+}
+
+const handleAxiosError = (error: any) => {
+    const errorResponse: AxiosError = error as AxiosError;
+    if (errorResponse.response) {
+        const errorMessage: { message: string } = errorResponse.response.data;
+        console.log("🚀 handleAxiosError ~ errorMessage", errorMessage)
+    }
+}
+
+const parseVideoDetailData = (response: any) => {
+    let responseData: VideoItemType[] = [];
+
+    if (response && isNonEmptyArray(response.rows)) {
+        responseData = response.rows;
+    }
+    return responseData
+}
+
+export const fetchVideoDetail = async (payload: FetchVideoDetailPayload) => {
+    try {
+        const response: any = await getCacheApiRequest(
+            `${BASE_URL}${VIDEO_DETAIL_ENDPOINT}${payload.nid}`
+        );
+        return response;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getVideoDetail = async (payload: FetchVideoDetailPayload) => {
+    try {
+        const response = await fetchVideoDetail(payload)
+        return parseVideoDetailData(response)
+    } catch (error) {
+        handleAxiosError(error)
+        return []
+    }
+}
