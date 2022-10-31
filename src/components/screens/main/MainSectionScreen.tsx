827945da97134859de904c6c7a0d18fe2cee4544
SrinivasanKannan18 <55490094+SrinivasanKannan18@users.noreply.github.com>
2022-10-31 17:24:10 +0530

    removed highlight tag (#934)

--- src/components/screens/main/MainSectionScreen.tsx
+++ src/components/screens/main/MainSectionScreen.tsx
@@ -614,8 +614,8 @@ export const MainSectionScreen = React.memo(({hidePlayerVisibility, tabIndex, cu
         <TopHeadLineNews data={headlineNews} />
       </View>
       {isNonEmptyArray(featuredArticleInfo) && <ArticleSection data={featuredArticleInfo} onUpdateBookmark={updateBookmarkInfo} />}
-      {isNonEmptyArray(gridViewSectionData) && <ArticleGridView data={gridViewSectionData} />}
-      {isNonEmptyArray(topViewSectionDataTwo) && <ArticleImageView data={topViewSectionDataTwo} />}
+      {isNonEmptyArray(gridViewSectionData) && <ArticleGridView showHighlightTitle={false} data={gridViewSectionData} />}
+      {isNonEmptyArray(topViewSectionDataTwo) && <ArticleImageView showHighlightTitle={false} data={topViewSectionDataTwo} />}
       {isNonEmptyArray(topViewSectionDataThree) && <ArticleImageView showImage={false} showHighlightTitle={false} data={topViewSectionDataThree} />}
       {isNonEmptyArray(infoGraphicBlock) && <InfoGraphicMapWidget 
         title={infoGraphicBlock[0].info}
