f0d4aa71e7a5cb8a2329da2a5d62c5945c46ed48
deepaktringapps <deepakkumar.r@tringapps.com>
2022-11-01 17:57:25 +0530

    Updated image url

--- src/components/molecules/AuthorItem.tsx
+++ src/components/molecules/AuthorItem.tsx
@@ -201,7 +201,7 @@ const AuthorItem = ({
             </View>
             <View>
                 <TouchableOpacity testID='AutherItemTO3' onPress={() => onPressWriter(authorId)}>
-                    <Image url={undefined} size={normalize(80)} resizeMode={'cover'} type={'round'}
+                    <Image url={image} size={normalize(80)} resizeMode={'cover'} type={'round'}
                         fallback={true}
                         fallbackName={ImagesName.authorDefault}
                     />
