5c0bbe3d858920b20bfd3edeb84a36b5b24f219c
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-01-06 16:50:47 +0530

    Removed flipper from react native config

--- react-native.config.js
+++ react-native.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  dependencies: {
-    ...(process.env.CI
-      ? { 'react-native-flipper': { platforms: { ios: null } } }
-      : {}),
-  },
   project: {
     ios: {},
     android: {},
