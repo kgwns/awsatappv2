285d28d4445f3a886c5bd2637d86aca23402a7c3
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-02-01 14:32:15 +0530

    develop merge

--- src/firebase/notification/__tests__/notification.test.tsx
+++ src/firebase/notification/__tests__/notification.test.tsx
@@ -1,52 +1,68 @@
-import {GetFCMToken} from '../notification'
-import { useNotificationSaveToken } from 'src/hooks';
+import {GetFCMToken} from '../notification';
+import {useNotificationSaveToken} from 'src/hooks';
 import DeviceInfo from 'react-native-device-info';
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import { fireEvent, RenderAPI } from '@testing-library/react-native'
-import { firebase } from '@react-native-firebase/analytics';
+import {render} from '@testing-library/react-native';
+import {fireEvent, RenderAPI} from '@testing-library/react-native';
 
-jest.mock('src/hooks/useNotificationSaveToken', () => ({useNotificationSaveToken: jest.fn()}));
-
-jest.mock('@react-native-firebase/app', () => {
-    return {
-      messaging: jest.fn(() => {
-        return {
-          onMessage: jest.fn(),
-          getToken: jest.fn(() => Promise.resolve('myMockToken'))
-        };
-      }),
-    };
+jest.mock('src/hooks/useNotificationSaveToken', () => ({
+  useNotificationSaveToken: jest.fn(),
+}));
+jest.mock('src/navigation/NavigationUtils', () => ({
+  navigate: jest.fn(),
+}));
+jest.mock('@react-native-firebase/messaging', () => {
+  return jest.fn().mockReturnValue({
+    getToken: jest.fn().mockResolvedValue(''),
+    requestPermission: jest.fn().mockResolvedValue(true),
+    registerDeviceForRemoteMessages: jest.fn(),
+    AuthorizationStatus: jest.fn(),
+    setBackgroundMessageHandler: jest.fn(),
+    getInitialNotification: jest.fn().mockResolvedValue(''),
+    onNotificationOpenedApp: jest.fn(),
+    onMessage: jest.fn(),
   });
-
+});
+jest.mock('@react-native-firebase/app', () => {
+  return {
+    messaging: jest.fn(() => {
+      return {
+        onMessage: jest.fn(),
+        getToken: jest.fn(() => Promise.resolve('myMockToken')),
+      };
+    }),
+  };
+});
 jest.mock('src/shared/utils/dimensions', () => ({
-    ...jest.requireActual('src/shared/utils/dimensions'),
-    isAndroid:true,
-  }));
+  ...jest.requireActual('src/shared/utils/dimensions'),
+  isAndroid: true,
+}));
 
 describe('<GetFCMToken>', () => {
-    const useNotificationSaveTokenMock = jest.fn();
-let instance:RenderAPI
-    beforeEach(() => {
-        (useNotificationSaveToken as jest.Mock).mockImplementation(useNotificationSaveTokenMock);
-        useNotificationSaveTokenMock.mockReturnValue({
-            saveTokenRequest:{
-                fcm_token: '',
-                platform: '',
-                device_name: '',
-            }
-          }); 
+  const useNotificationSaveTokenMock = jest.fn();
+  let instance: RenderAPI;
+  const navigate = jest.fn();
+  beforeEach(() => {
+    (useNotificationSaveToken as jest.Mock).mockImplementation(
+      useNotificationSaveTokenMock,
+    );
+    (navigate as jest.Mock).mockReturnValueOnce(navigate);
+    useNotificationSaveTokenMock.mockReturnValue({
+      saveTokenRequest: {
+        fcm_token: '',
+        platform: '',
+        device_name: '',
+      },
+    });
 
-          const component = (
-            <GetFCMToken />
-        );
-        instance = render(component);
-    })
-    afterEach(() => {
-        jest.clearAllMocks()
-        instance.unmount()
-    })
-    it('Should render component', () => {
-        expect(instance).toBeDefined()
-    })
-})
+    const component = <GetFCMToken />;
+    instance = render(component);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    instance.unmount();
+  });
+  it('Should render component', () => {
+    expect(instance).toBeDefined();
+  });
+});
--- src/firebase/notification/notification.tsx
+++ src/firebase/notification/notification.tsx
@@ -1,176 +1,187 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import messaging from '@react-native-firebase/messaging';
 import '@react-native-firebase/messaging';
 import {Alert, Platform} from 'react-native';
 import DeviceInfo from 'react-native-device-info';
-import { useNotificationSaveToken } from 'src/hooks';
-import { SaveTokenBodyType } from 'src/redux/notificationSaveToken/types';
+import {useNotificationSaveToken} from 'src/hooks';
+import {SaveTokenBodyType} from 'src/redux/notificationSaveToken/types';
 import firebase from '@react-native-firebase/app';
-import { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+import {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
 import PushNotification from 'react-native-push-notification';
 import PushNotificationIOS from '@react-native-community/push-notification-ios';
-import { isAndroid, isIOS } from 'src/shared/utils';
-import { ScreensConstants } from 'src/constants/Constants';
-import { navigate, navigationRefernce } from 'src/navigation/NavigationUtils';
-import notifee, { AndroidImportance, EventType } from '@notifee/react-native';
-import { CustomAlert } from 'src/shared/utils/utilities';
+import {isAndroid, isIOS} from 'src/shared/utils';
+import {ScreensConstants} from 'src/constants/Constants';
+import {navigate, navigationRefernce} from 'src/navigation/NavigationUtils';
+import notifee, {AndroidImportance, EventType} from '@notifee/react-native';
 
-async function onDisplayNotification(remoteMessage: FirebaseMessagingTypes.RemoteMessage) {
-    const channelId = await notifee.createChannel({
-        id: `${remoteMessage?.data?.locKey}`,
-        name: 'Awsatapp',
-        importance: AndroidImportance.HIGH,
-    });
+async function onDisplayNotification(
+  remoteMessage: FirebaseMessagingTypes.RemoteMessage,
+) {
+  const channelId = await notifee.createChannel({
+    id: `${remoteMessage?.data?.locKey}`,
+    name: 'Awsatapp',
+    importance: AndroidImportance.HIGH,
+  });
 
+  await notifee.displayNotification({
+    data: remoteMessage?.data,
+    title: remoteMessage.notification?.title,
+    body: remoteMessage.notification?.body,
+    android: {
+      channelId,
+      pressAction: {
+        id: 'default',
+      },
+    },
 
-    await notifee.displayNotification({
-        data: remoteMessage?.data,  
-        title: remoteMessage.notification?.title,
-        body: remoteMessage.notification?.body, 
-        android: {
-            channelId,
-            pressAction: {
-                id: 'default',
-            },
-        },
-
-        ios: {
-            critical: true,
-        }
-    });
+    ios: {
+      critical: true,
+    },
+  });
 }
 
 export const GetFCMToken = () => {
-    const { saveTokenRequest } = useNotificationSaveToken();
-    var deviceOS = Platform.OS;
-    var deviceName = DeviceInfo.getDeviceId();
+  const {saveTokenRequest} = useNotificationSaveToken();
+  var deviceOS = Platform.OS;
+  var deviceName = DeviceInfo.getDeviceId();
 
-    const getToken = () => {
-        messaging()
-        .getToken()
-        .then((x) => {
-            CustomAlert({
-                message: 'x'
-              })  
-             console.log('token',x)       
-             var requestObject: SaveTokenBodyType = {
-                fcm_token: x,
-                platform: deviceOS,
-                device_name: deviceName
-            }
-            saveTokenRequest(requestObject);
-        })
-        .catch(e => console.log(e));
-    };
+  const getToken = () => {
+    messaging()
+      .getToken()
+      .then(x => {
+        var requestObject: SaveTokenBodyType = {
+          fcm_token: x,
+          platform: deviceOS,
+          device_name: deviceName,
+        };
+        saveTokenRequest(requestObject);
+      })
+      .catch(e => console.log(e));
+  };
 
-    const onOpenNotification =  (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {        
-        console.log('notify', remoteMessage);
-        if (remoteMessage && remoteMessage.data && remoteMessage.data?.type == "article") {
-            navigate(ScreensConstants.ARTICLE_DETAIL_SCREEN, {nid: remoteMessage.data.id});
-        } else if (remoteMessage && remoteMessage.data && remoteMessage.data?.type == "opinion") {
-            navigate(ScreensConstants.OPINION_ARTICLE_DETAIL_SCREEN, { nid: remoteMessage.data.id })
-        }
-        else if (remoteMessage && remoteMessage.data && remoteMessage.data?.type == "dynamic-section") {
-            navigate(ScreensConstants.SectionArticlesParentScreen, { title: remoteMessage.data.title, 
-                keyName: remoteMessage.data.keyname,
-                sectionId:remoteMessage.data.sectionid
-            })
-        }
+  const onOpenNotification = (
+    remoteMessage: FirebaseMessagingTypes.RemoteMessage,
+  ) => {
+    if (
+      remoteMessage &&
+      remoteMessage.data &&
+      remoteMessage.data?.type == 'article'
+    ) {
+      navigate(ScreensConstants.ARTICLE_DETAIL_SCREEN, {
+        nid: remoteMessage.data.id,
+      });
+    } else if (
+      remoteMessage &&
+      remoteMessage.data &&
+      remoteMessage.data?.type == 'opinion'
+    ) {
+      navigate(ScreensConstants.OPINION_ARTICLE_DETAIL_SCREEN, {
+        nid: remoteMessage.data.id,
+      });
+    } else if (
+      remoteMessage &&
+      remoteMessage.data &&
+      remoteMessage.data?.type == 'dynamic-section'
+    ) {
+      navigate(ScreensConstants.SectionArticlesParentScreen, {
+        title: remoteMessage.data.title,
+        keyName: remoteMessage.data.keyName,
+        sectionId: remoteMessage.data.sectionId,
+      });
     }
-    
-    useEffect(() => {
-        messaging().setBackgroundMessageHandler(async remoteMessage => {
-            onOpenNotification(remoteMessage)
-        });
-       
-        getToken();
-      
-        messaging()
-            //When Application open from quit state
-            .getInitialNotification()
-            .then(remoteMessage => {
-                onOpenNotification(remoteMessage!)
-            });
-        notifee.onForegroundEvent(async({ type, detail }) => {
-            const { notification, pressAction } = detail;
-            switch (type) {
-                case EventType.DISMISSED:
-                  console.log('User dismissed notification', notification?.data!);
-                  break;
-                case EventType.PRESS:
-                  console.log('User pressed notification', notification?.data!);
-                    onOpenNotification(notification)
-                  break;
-              }
-          })
+  };
 
-          notifee.onBackgroundEvent(async({ type, detail }) => {
-            const { notification, pressAction } = detail;
-            switch (type) {
-                case EventType.DISMISSED:
-                  console.log('User dismissed notification', notification?.data!);
-                  break;
-                case EventType.PRESS:
-                  console.log('User pressed notification', notification?.data!);
-                    onOpenNotification(notification)
-                  break;
-              }
-          })
-        messaging().onNotificationOpenedApp(remoteMessage => {
-            console.log('notifeeeeee', "onNotificationOpenedApp")
-        });
+  useEffect(() => {
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      onOpenNotification(remoteMessage);
+    });
 
-        const unsubscribe = messaging().onMessage(async remoteMessage => {
-            onDisplayNotification(remoteMessage)
-        });
+    getToken();
 
-        return unsubscribe;
-    }, []);
+    messaging()
+      //When Application open from quit state
+      .getInitialNotification()
+      .then(remoteMessage => {
+        onOpenNotification(remoteMessage!);
+      });
+    notifee.onForegroundEvent(async ({type, detail}) => {
+      const {notification, pressAction} = detail;
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('User dismissed notification', notification?.data!);
+          break;
+        case EventType.PRESS:
+          onOpenNotification(notification);
+          break;
+      }
+    });
 
+    notifee.onBackgroundEvent(async ({type, detail}) => {
+      const {notification, pressAction} = detail;
+      switch (type) {
+        case EventType.DISMISSED:
+          console.log('User dismissed notification', notification?.data!);
+          break;
+        case EventType.PRESS:
+          onOpenNotification(notification);
+          break;
+      }
+    });
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log('notifeeeeee', 'onNotificationOpenedApp');
+    });
 
-    useEffect(() => {
-        if (!isIOS) {
-            return
-        }
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      onDisplayNotification(remoteMessage);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (!isIOS) {
+      return;
+    }
     messaging().onMessage(response => {
-        PushNotificationIOS.requestPermissions().then(() =>
-        showNotification(response.notification!),
+      PushNotificationIOS.requestPermissions().then(
+        () => showNotification(response.notification!),
         // onDisplayNotification(response.data!)
+      );
+    });
+
+    PushNotificationIOS.addEventListener('localNotification', notification => {
+      onOpenNotification(notification);
+    });
+
+    PushNotificationIOS.addEventListener(
+      'notification',
+      function (notification) {
+        onOpenNotification(notification);
+      },
     );
-});
-        PushNotificationIOS.addEventListener('register', token => {
-            console.log("Token>>>>>>>>>>>>>>>>>>>>>>>>>>>>", token)
-            getToken();
-        })
 
-        PushNotificationIOS.addEventListener('localNotification', (notification) => {
-            console.log("localNotification>>>>>>>>>>>>>>>>>>>>>>>>>>>>", notification)
-            onOpenNotification(notification)
-        })
+    return () => {
+      PushNotificationIOS.removeEventListener('register');
+      PushNotificationIOS.removeEventListener('registrationError');
+      PushNotificationIOS.removeEventListener('notification');
+    };
+  }, []);
 
-        PushNotificationIOS.addEventListener('notification', function (notification) {
-            console.log("notification>>>>>>>>>>>>>>>>>>>>>>>>>>>>", notification)
-            onOpenNotification(notification)
-        })
-        
-        return () => {
-            PushNotificationIOS.removeEventListener('register')
-            PushNotificationIOS.removeEventListener('registrationError')
-            PushNotificationIOS.removeEventListener('notification')
-        }
-    }, [])
-
-    const showNotification = (notification: FirebaseMessagingTypes.Notification) => {
-        PushNotification.localNotification({ title: notification.title, message: notification.body!,channelId: notification.android?.channelId });
-        console.log('DataNotification',notification);
-    }; 
+  const showNotification = (
+    notification: FirebaseMessagingTypes.Notification,
+  ) => {
+    PushNotification.localNotification({
+      title: notification.title,
+      message: notification.body!,
+      channelId: notification.android?.channelId,
+    });
+  };
   return <></>;
 };
 
 export const registerBackgroundPushNotification = () => {
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-        console.log('Message handled in the background!', remoteMessage);
-        onDisplayNotification(remoteMessage)
-    });
-}
+  // Register background handler
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    onDisplayNotification(remoteMessage);
+  });
+};
