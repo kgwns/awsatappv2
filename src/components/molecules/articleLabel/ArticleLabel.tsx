1511dee1753ccd6ac7a0f88e64ec32540e98603f
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-03-08 20:01:51 +0530

    adjusted styles

--- src/components/molecules/articleLabel/ArticleLabel.tsx
+++ src/components/molecules/articleLabel/ArticleLabel.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Label, LabelTypeProp, LiveBlogTag } from 'src/components/atoms';
-import { isNotEmpty, normalize } from 'src/shared/utils';
+import { Label, LiveBlogTag } from 'src/components/atoms';
+import { isNotEmpty, isTab, normalize } from 'src/shared/utils';
 import { DisplayTypes, TranslateConstants, TranslateKey } from 'src/constants/Constants';
 import { Styles } from 'src/shared/styles';
 import { fonts } from 'src/shared/styles/fonts';
@@ -34,7 +34,6 @@ export const ArticleLabel: FC<ArticleLabelTypes> = ({
                 <View style={[labelContainerStyle, { backgroundColor: bgColor }]}>
                     <Label children={tagName}
                         style={style.tagText}
-                        labelType={LabelTypeProp.caption3}
                     />
                 </View>
             </View>
@@ -67,9 +66,11 @@ const style = StyleSheet.create({
         paddingHorizontal: normalize(7),
         color: Styles.color.white,
         fontFamily: fonts.Effra_Arbc_Regular,
+        fontSize: 12,
+        lineHeight: 18,
     },
     topMargin: {
-        marginTop: 10
+        marginTop: isTab ? 0 : 10
     },
     bottomMargin: {
         marginBottom: 10
