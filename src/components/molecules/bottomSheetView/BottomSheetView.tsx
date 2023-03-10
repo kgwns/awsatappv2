1fa482b676b0bc7149a22808e5b7bbebe9f2f10c
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-03-10 12:30:57 +0530

    fixed AMAR-1146 missed issues

--- src/components/molecules/bottomSheetView/BottomSheetView.tsx
+++ src/components/molecules/bottomSheetView/BottomSheetView.tsx
@@ -1,6 +1,6 @@
 import { View, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
-import { isTab, screenWidth, normalize, isIOS } from 'src/shared/utils';
+import { isTab, screenWidth, normalize, isIOS, screenHeight } from 'src/shared/utils';
 import { Label } from 'src/components/atoms';
 import { useThemeAwareObject } from 'src/shared/styles/useThemeAware';
 import { colors, CustomThemeType } from 'src/shared/styles/colors';
@@ -53,8 +53,10 @@ export const BottomSheetView = ({ onPressSignUp, title, subTitle, description, s
                     <Label style={StyleSheet.flatten([isPortrait() ? style.buttonLabel : style.buttonLabelLandscape, { color: colors.black }])}>{logInLabel}</Label>
                 </TouchableOpacity>
             </View>
-            <View style={isPortrait() ? style.popupImageContainer : style.popupImageContainerLandscape}>
-                <AlertImage/>
+            <View style={style.popupImageWrapper}>
+                <View style={isPortrait() ? style.popupImageContainer : style.popupImageContainerLandscape}>
+                    <AlertImage />
+                </View>
             </View>
         </View>
     )
@@ -70,17 +72,17 @@ const customStyle = (theme: CustomThemeType) => {
         },
         title: {
             fontFamily: fonts.AwsatDigital_Black,
-            fontSize: normalize(30),
-            lineHeight: normalize(53),
+            fontSize: 30,
+            lineHeight: 53,
             color: colors.carouselPink,
             alignSelf: 'center',
             textAlign: 'center',
-            marginTop: normalize(25)
+            marginTop: 20
         },
         titleLandscape: {
             fontFamily: fonts.AwsatDigital_Black,
-            fontSize: normalize(20),
-            lineHeight: normalize(33),
+            fontSize: 20,
+            lineHeight: 33,
             color: colors.carouselPink,
             alignSelf: 'center',
             textAlign: 'center',
@@ -111,8 +113,8 @@ const customStyle = (theme: CustomThemeType) => {
         },
         description: {
             fontFamily: fonts.Effra_Arbc_Regular,
-            fontSize: 19,
-            lineHeight: isIOS ? 32 : 38,
+            fontSize: 16,
+            lineHeight: isIOS ? 29 : 35,
             color: colors.carouselPink,
             alignSelf: 'center',
             textAlign: 'center',
@@ -174,7 +176,7 @@ const customStyle = (theme: CustomThemeType) => {
         },
         logo: {
             width: normalize(250),
-            height: normalize(45),
+            height: 0.055 * screenHeight,
         },
         logoLandscape: {
             width: normalize(200),
@@ -183,7 +185,7 @@ const customStyle = (theme: CustomThemeType) => {
         popupImageContainer: {
             alignSelf: 'center',
             width: normalize(250),
-            height: normalize(243),
+            height: '78%',
             marginTop: normalize(35)
         },
         popupImageContainerLandscape: {
@@ -194,7 +196,10 @@ const customStyle = (theme: CustomThemeType) => {
         popupImage: {
             width: '100%',
             height: '100%'
-        }
+        },
+        popupImageWrapper: {
+            flex: 0.95
+        },
     })
     return bottomSheetViewStyle
 }
