f86ca3919b0de95b55f9f55f1006e0c2388a263c
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-03-24 13:48:13 +0530

    Video screen orientation lock configured only for mobile

--- src/components/molecules/videoPlayer/VideoPlayer.tsx
+++ src/components/molecules/videoPlayer/VideoPlayer.tsx
@@ -4,7 +4,7 @@ import {useThemeAwareObject} from 'src/shared/styles/useThemeAware';
 import {colors} from 'src/shared/styles/colors';
 import Orientation, {OrientationType} from 'react-native-orientation-locker';
 import {Edge} from 'react-native-safe-area-context';
-import {horizontalEdge, isAndroid, isNotEmpty} from 'src/shared/utils';
+import {horizontalEdge, isAndroid, isNotEmpty, isTab} from 'src/shared/utils';
 import {ScreenContainer} from 'src/components/screens';
 import SystemNavigationBar from 'react-native-system-navigation-bar';
 import VideoPlayerFullScreen from './VideoPlayerFullScreen';
@@ -33,10 +33,14 @@ export const VideoPlayerComponent: FunctionComponent<VideoPlayerProps> = ({
           deviceOrientation === OrientationType['LANDSCAPE-LEFT'] ||
           deviceOrientation === OrientationType['LANDSCAPE-RIGHT']
         ) {
-          Orientation.lockToLandscape();
+          if(!isTab) {
+            Orientation.lockToPortrait();
+          }
           setFullScreen(true);
         } else if (deviceOrientation === OrientationType['PORTRAIT']) {
-          Orientation.lockToPortrait();
+          if(!isTab) {
+            Orientation.lockToPortrait();
+          }
           setFullScreen(false);
         }
       }
@@ -46,7 +50,9 @@ export const VideoPlayerComponent: FunctionComponent<VideoPlayerProps> = ({
   const goBackToScreen = () => {
     if (goBack) {
       if(fullScreen){
-        Orientation.lockToPortrait();
+        if(!isTab) {
+          Orientation.lockToPortrait();
+        }
         setFullScreen(false);
       }
       setIsPaused(true);
@@ -77,14 +83,16 @@ export const VideoPlayerComponent: FunctionComponent<VideoPlayerProps> = ({
     Orientation.addDeviceOrientationListener(changeOrientation);
     return () => {
       Orientation.removeDeviceOrientationListener(changeOrientation);
-      Orientation.lockToPortrait();
+      if(!isTab) {
+        Orientation.lockToPortrait();
+      }
       StatusBar.setHidden(false);
       isAndroid && SystemNavigationBar.navigationShow();
     };
   }, []);
 
   const changeFullScreen = () => {
-    fullScreen ? Orientation.lockToPortrait() : Orientation.lockToLandscape();
+    fullScreen ? isTab ? Orientation.unlockAllOrientations() : Orientation.lockToPortrait() : Orientation.lockToLandscape();
     setFullScreen(!fullScreen);
   };
 
