73af3106581cb3b3a0605c3d03cea31d5d84e2b9
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-03-30 20:16:52 +0530

    Updated styles for contact us titles

--- src/navigation/CustomDrawerContent.tsx
+++ src/navigation/CustomDrawerContent.tsx
@@ -366,6 +366,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
   }
 
   const socialIconSize = isTab ? 33 : 23;
+  const contactUsTitleStyle = isTab ? styles.tabletTitleStyle : styles.nonBoldTitle; 
 
   return (
     <ScreenContainer showPlayer={false} backgroundColor={styles.screenBackgroundColor?.backgroundColor}>
@@ -402,7 +403,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
               ScreensConstants.TERMS_AND_ABOUT_US,
               { title: ADVERTISE_WITH_US, id: ADVERTISE_INFO_ID }
             )}
-            titleStyle={styles.nonBoldTitle}
+            titleStyle={contactUsTitleStyle}
           />
           {/* <ButtonList
             title={t('drawer.aboutTheEast')}
@@ -410,7 +411,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
               ScreensConstants.TERMS_AND_ABOUT_US,
               { title: t('drawer.aboutTheEast'), id: AWSATT_HISTORY_INFO_ID }
             )}
-            titleStyle={styles.nonBoldTitle}
+            titleStyle={contactUsTitleStyle}
           /> */}
           <ButtonList
             title={ABOUT_THE_MIDDLE_EAST}
@@ -418,7 +419,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
               ScreensConstants.TERMS_AND_ABOUT_US,
               { title: ABOUT_THE_MIDDLE_EAST, id: ABOUT_US }
             )}
-            titleStyle={styles.nonBoldTitle}
+            titleStyle={contactUsTitleStyle}
           />
           <ButtonList
             title={CALL_US}
@@ -426,7 +427,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
               ScreensConstants.CONTACT_US_SCREEN, {}
 
             )}
-            titleStyle={styles.nonBoldTitle}
+            titleStyle={contactUsTitleStyle}
           />
           <ButtonList
             title={TERMS_OF_USE}
@@ -434,7 +435,7 @@ const CustomDrawerContent = (props: CustomDrawerContentProps) => {
               ScreensConstants.TERMS_AND_ABOUT_US,
               { title: TERMS_OF_USE, id: TERMS_AND_CONDITION }
             )}
-            titleStyle={styles.nonBoldTitle}
+            titleStyle={contactUsTitleStyle}
           />
 
           <View style={styles.socialContainer}>
@@ -618,5 +619,12 @@ const createStyles = (theme: CustomThemeType) =>
     },
     screenBackgroundColor: {
       backgroundColor: theme.profileBackground
-    }
+    },
+    tabletTitleStyle: {
+      fontFamily: fonts.Effra_Arbc_Regular,
+      fontSize: 20,
+      lineHeight: 35,
+      fontWeight:"400",
+      color: theme.contactUsTitleColor,
+    },
   });
--- src/shared/styles/colors.ts
+++ src/shared/styles/colors.ts
@@ -101,6 +101,7 @@ export const colors = {
   black55: '#0000008C',
   lightAlterGray: '#D1DEDD',
   blackOpacity60: '#00000099',
+  darkFungusGreen: '#172F2F',
 };
 
 export interface CustomThemeType {
@@ -164,6 +165,7 @@ export interface CustomThemeType {
   onBoardBackground: string,
   newsFeed: string,
   articleCaption: string,
+  contactUsTitleColor: string,
 }
 
 export const LIGHT_THEME_ID = 'light'
@@ -230,6 +232,7 @@ export const DEFAULT_LIGHT_THEME: CustomThemeType = {
   onBoardBackground: colors.aquaHaze,
   newsFeed: colors.smokeyGrey,
   articleCaption: colors.charcoalGray,
+  contactUsTitleColor: colors.darkFungusGreen,
 }
 
 export const DEFAULT_DARK_THEME: CustomThemeType = {
@@ -293,5 +296,6 @@ export const DEFAULT_DARK_THEME: CustomThemeType = {
   onBoardBackground: colors.codGray,
   newsFeed: colors.white,
   articleCaption: colors.white,
+  contactUsTitleColor: colors.white,
 }
 
