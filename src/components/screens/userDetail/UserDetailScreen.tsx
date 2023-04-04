fb371fad1eb883d885861be7d04a192986bc6424
DeepakTringapps <deepakkumar.r@tringapps.com>
2023-04-04 12:31:30 +0530

    Fixed AMAR-1226 iPad user screen inner scroll

--- src/components/screens/userDetail/UserDetailScreen.tsx
+++ src/components/screens/userDetail/UserDetailScreen.tsx
@@ -725,7 +725,7 @@ const createStyles = (theme: CustomThemeType) =>
     },
     userContainerStyle: {
       flex: 1,
-      marginVertical: '20%',
+      marginVertical: isTab ? '10%' : '20%',
     },
     userContainer: {
       flexDirection: 'row',
