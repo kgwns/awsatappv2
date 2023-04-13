64421ba52840f29ca2e20e90dfa8805d6ec3d9e4
DeepakTringapps <deepakkumar.r@tringapps.com>
2023-04-13 19:59:39 +0530

    Fixed AMAR-1242 Section combo article duplication issue

--- src/services/apiEndPoints.ts
+++ src/services/apiEndPoints.ts
@@ -25,6 +25,7 @@ export const HORIZONTAL_ARTICLE_END_POINT = 'api/v2/home/topview/tghtyt_khast'
 export const EDITORS_CHOICE = 'api/v2/home/editorschoice';
 export const SPOTLIGHT_COMBO = 'api/v2/home/spotlight';
 export const INFO_GRAPHIC_BLOCK = 'api/block/v1/top';
+export const SECTION_COMBO_TWO = 'api/v2/user/articlesection/11+97115+97110';
 export const SECTION_COMBO_SIX = '/api/v2/home/thakafa';
 export const ARCHIVED_ARTICLE_ENDPOINT = 'api/v2/archivedarticle'
 
