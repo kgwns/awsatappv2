4d92c67fe6d0a22bb77ebe46b8e85a39c6463a7e
vigneshrajs <vigneshraj.s@tringapps.com>
2023-04-28 13:40:16 +0530

    testCase Issue fixed

--- src/components/organisms/InfoGraphicMapWidget.tsx
+++ src/components/organisms/InfoGraphicMapWidget.tsx
@@ -9,12 +9,12 @@ import { generateAssetFontCss } from '../screens/articleDetail/components/Articl
 import { useTheme } from 'src/shared/styles/ThemeProvider';
 
 type InfoGraphicMapWidgetProps = {
-    headerTitle: string
+    title: string
     htmlContent: string
 }
 
 const InfoGraphicMapWidget = ({
-    headerTitle = '', htmlContent
+    title = '', htmlContent
 }: InfoGraphicMapWidgetProps) => {
 
     const infoGraphicHTML = ({ body }: { body: string }) => `
@@ -61,7 +61,7 @@ const InfoGraphicMapWidget = ({
 
     const widgetHeaderData: WidgetHeaderProps = {
         headerLeft: {
-          title: headerTitle,
+          title: title,
           color: themeData.primaryBlack,
           labelType: LabelTypeProp.title3,
           textStyle: { fontFamily: fonts.AwsatDigital_Black }
