b61b3a911eeb1036d509874b3b91c66220d062cf
Priyadharshini <priyadharshini.th@tringapps.com>
2023-05-24 15:25:21 +0530

    UI changes added for iPad and Tab for KeepNotifiedScreen

--- src/components/molecules/stepLineCircle/StepLineCircle.tsx
+++ src/components/molecules/stepLineCircle/StepLineCircle.tsx
@@ -14,7 +14,7 @@ export interface StepLineCircleProps {
 
 export const StepLineCircle: FunctionComponent<StepLineCircleProps> = ({
     currentStep,
-    totalStep = 3,
+    totalStep = 4,
 }) => {
     const style = useThemeAwareObject(stepLineCircleStyle);
     const { theme } = useAppCommon()
