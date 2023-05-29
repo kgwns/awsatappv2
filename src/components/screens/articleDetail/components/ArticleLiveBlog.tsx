b5e0c462a1cc4b901d54268740792bf67c57cf12
SrinivasanKannan18 <srinivasan.kannan@tringapps.com>
2023-05-29 16:59:58 +0530

    Added onShouldStartLoadWithRequest for iOS live blog

--- src/components/screens/articleDetail/components/ArticleLiveBlog.tsx
+++ src/components/screens/articleDetail/components/ArticleLiveBlog.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, ScrollView, View, Linking } from 'react-native'
 import React from 'react'
-import { isIOS, screenWidth } from 'src/shared/utils'
+import { isAndroid, isIOS, screenWidth } from 'src/shared/utils'
 import { useThemeAwareObject } from 'src/shared/styles/useThemeAware'
 import AutoHeightWebView from 'react-native-autoheight-webview'
 import { LIVE_BLOG_URL } from 'src/services/apiUrls'
@@ -17,8 +17,15 @@ export const ArticleLiveBlog = React.memo(({
     let webviewRef: any = React.createRef();
     const liveBlogUrl = `${LIVE_BLOG_URL}${scribbleId}`;
 
+    const onNavigationStateChange = (event: any) => {
+        if (event.url !== liveBlogUrl && isIOS) {
+            webviewRef.stopLoading();
+            Linking.openURL(event.url);
+        }
+    }
+
     const onShouldStartLoadWithRequest = (event: any) => {
-        if (event.url !== liveBlogUrl) {
+        if (event.url !== liveBlogUrl && isAndroid) {
             webviewRef.stopLoading();
             Linking.openURL(event.url);
             return false
@@ -39,6 +46,7 @@ export const ArticleLiveBlog = React.memo(({
             androidLayerType="hardware"
             allowsFullscreenVideo={true}
             scrollEnabled={false}
+            onNavigationStateChange={onNavigationStateChange}
             onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
             scalesPageToFit={false}
             setBuiltInZoomControls={false}
