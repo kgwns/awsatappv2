40cb0efd3b12ba2f9ea9cffbe04ce2e491e5e2e7
Liwaa Awar <Liwaa-Awar@live.com>
2024-02-27 22:25:58 +0100

    Migrate to React Native 0.72

--- android/app/src/main/java/com/awsatapp/MainActivity.java
+++ android/app/src/main/java/com/awsatapp/MainActivity.java
@@ -1,5 +1,10 @@
 package com.awsatapp;
 
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,7 +19,6 @@ import com.awsatapp.reactPackage.Constant;
 import com.awsatapp.reactPackage.MyContextWrapper;
 import com.awsatapp.reactPackage.listener.ThemeChangeEvent;
 import com.awsatapp.reactPackage.manager.CoreCacheManager;
-import com.facebook.react.ReactActivity;
 
 import org.devio.rn.splashscreen.SplashScreen;
 
@@ -85,5 +89,19 @@ public class MainActivity extends ReactActivity{
   @Override
   protected String getMainComponentName() {
     return "Awsatapp";
+  }
+
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+   * (aka React 18) with two boolean flags.
+   */
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
   }
 }
--- android/app/src/main/java/com/awsatapp/MainApplication.java
+++ android/app/src/main/java/com/awsatapp/MainApplication.java
@@ -2,7 +2,6 @@ package com.awsatapp;
 
 import android.app.Application;
 import android.content.ComponentCallbacks2;
-import android.content.Context;
 import android.content.Intent;
 import android.os.StatFs;
 import android.provider.Settings;
@@ -14,12 +13,12 @@ import com.awsatapp.reactPackage.manager.FileDownloadSerialQueue;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.proyecto26.inappbrowser.RNInAppBrowserPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
@@ -33,7 +32,7 @@ public class MainApplication extends Application implements ReactApplication, Co
     }
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new  DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -55,6 +54,15 @@ public class MainApplication extends Application implements ReactApplication, Co
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+        @Override
+        protected Boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
       };
 
   @Override
@@ -72,7 +80,11 @@ public class MainApplication extends Application implements ReactApplication, Co
       sharedI18nUtilInstance.forceRTL(this,true);
       sharedI18nUtilInstance.allowRTL(this, true);
       checkForLowStorage();
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+        // If you opted-in for the New Architecture, we load the native entry point for this app.
+        DefaultNewArchitectureEntryPoint.load();
+      }
+      ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
     @Override
@@ -130,37 +142,6 @@ public class MainApplication extends Application implements ReactApplication, Co
     public void onLowMemory() {
         super.onLowMemory();
     }
-
-    /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.awsatapp.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
 
     private void checkForLowStorage() {
         long mFreeMem = getDeviceCurrentStorage();
